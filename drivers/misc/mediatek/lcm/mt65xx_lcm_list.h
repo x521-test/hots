7ac0fc1640c3fe191cbcc2d524ef515efcc114e8
raymondmiracle <34572535+raymondmiracle@users.noreply.github.com>
2019-03-14 15:57:45 +0100

    hots : Add lcm

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -1,20 +1,8 @@
-/*
- * Copyright (C) 2015 MediaTek Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- */
-
 #ifndef __MT65XX_LCM_LIST_H__
 #define __MT65XX_LCM_LIST_H__
 
 #include <lcm_drv.h>
+<<<<<<< HEAD
 
 extern LCM_DRIVER hct_rm68200_dsi_vdo_hd_cpt;
 extern LCM_DRIVER hct_otm1285a_dsi_vdo_hd_boe;
@@ -289,6 +277,9 @@ extern LCM_DRIVER auo_wuxga_dsi_vdo_lcm_drv;
 extern LCM_DRIVER ili9806e_dsi_vdo_tcl_blu5039_drv;
 extern LCM_DRIVER otm8019a_dsi_vdo_boyi_t50m_drv;
 extern LCM_DRIVER ili9881c_hd_dsi_vdo_ilitek_nt50358_3lane_lcm_drv;
+=======
+extern LCM_DRIVER ili9881c_hd720_boe5p2_dj_5080_lcm_drv;
+>>>>>>> c74e2d66... Hots : add lcm
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
 #endif
